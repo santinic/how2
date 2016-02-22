f12d47fb238b976485a1963e3490cc906e6bc5fb
Claudio Santini <vuotomeccanico@gmail.com>
2016-02-22 18:34:01 +0100

    fixed matched[1] bug

--- test/parse-question-link.js
+++ test/parse-question-link.js
@@ -5,14 +5,14 @@ describe('parseStackoverflowQuestionId', function() {
     it('should not recognize empty strings', function () {
         assert.equal(fun(''), null);
     });
-    xit('should not recognize links that are not from stackexchange/overflow', function () {
+    it('should not recognize links that are not from stackexchange/overflow', function () {
         var url = 'http://someother.com/questions/2342/title';
         assert.equal(fun(url), null);
     });
     it('should parse stackoverflow links', function () {
         var url = 'http://stackoverflow.com/questions/39934/is-it-possible-to';
         assert.deepEqual(fun(url), {
-            site: null,
+            site: 'stackoverflow',
             questionId: '39934'
         });
     });
@@ -22,5 +22,9 @@ describe('parseStackoverflowQuestionId', function() {
             site: 'unix',
             questionId: '39934'
         });
+    });
+    it('should return nul on links without id', function () {
+        var url = 'http://unix.stackexchange.com/questions/tagged/read?sort=votes&pageSize=50';
+        assert.deepEqual(fun(url), null);
     });
 });
