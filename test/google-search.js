48fc619c5f18abd00537c93d5ecbb226844eadc6
Paolo Cifariello <paolocifa@gmail.com>
2018-09-23 20:38:02 +0100

    Switch to async/await.

--- test/google-search.js
+++ test/google-search.js
@@ -1,9 +1,9 @@
 var assert = require('chai').assert;
-var search = require('../lib/how2').search;
+var googleSearch = require('../lib/how2').googleSearch;
 
 describe('search', () => {
     it('should find tail -f', (done) => {
-        search('read file while is changing', undefined).then(({links}) => {
+        googleSearch('read file while is changing', undefined).then(({links}) => {
             assert.isAbove(links.length, 5);
             assert.isTrue(links[0].title.indexOf('Output file contents while they change') !== -1);
             done();
@@ -11,7 +11,7 @@ describe('search', () => {
     });
 
     it('should not find any result', (done) => {
-        search('', null).then(({links}) => {
+        googleSearch('', null).then(({links}) => {
             assert.lengthOf(links, 0);
             done();
         }).catch((err) => assert(false));
