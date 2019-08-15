eeafbdc510c098d3e5abba4d774aead486949820
Vladimir Mikulic <vladimir.mikulic2@gmail.com>
2019-08-15 16:33:39 +0200

    UPDATE google-search test

--- test/google-search.js
+++ test/google-search.js
@@ -2,18 +2,24 @@ var assert = require('chai').assert;
 var googleSearch = require('../lib/how2').googleSearch;
 
 describe('search', () => {
-    it('should find tail -f', (done) => {
-        googleSearch('read file while is changing', undefined).then(({links}) => {
-            assert.isAbove(links.length, 5);
-            assert.isTrue(links[0].title.indexOf('Output file contents while they change') !== -1);
-            done();
-        }).catch((err) => assert(false));
-    });
+  it('should find tail -f', done => {
+    googleSearch('read file while is changing')
+      .then(({ links, titles }) => {
+        assert.isAbove(links.length, 5);
+        assert.isTrue(
+          titles[0].indexOf('Output file contents while they change') !== -1
+        );
+        done();
+      })
+      .catch(err => assert(false));
+  });
 
-    it('should not find any result', (done) => {
-        googleSearch('', null).then(({links}) => {
-            assert.lengthOf(links, 0);
-            done();
-        }).catch((err) => assert(false));
-    });
+  it('should not find any result', done => {
+    googleSearch('')
+      .then(({ links }) => {
+        assert.lengthOf(links, 0);
+        done();
+      })
+      .catch(err => assert(false));
+  });
 });
