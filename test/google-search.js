f26de61baa39ee138f61c36af23826893fb8f6b8
santinic <hireclaudio@gmail.com>
2022-08-19 23:22:40 +0100

    test fix

--- test/google-search.js
+++ test/google-search.js
@@ -3,10 +3,8 @@ const { googleSearch } = require('../lib/how2')
 
 describe('search', () => {
   it('should find tail -f', (done) => {
-    googleSearch('read file while is changing', undefined).then((searchResults) => {
-      console.log(searchResults)
-      assert.isAbove(searchResults.length, 5)
-      assert.isTrue(searchResults[0].title.indexOf('Output file contents while they change') !== -1)
+    googleSearch('read file while is changing', undefined).then((results) => {
+      assert.include((results.map(r => r.snippet).join(' | ')), 'tail -f')
       done()
     }).catch((err) => done(err))
   })
