fb4ef65a350461479ea58ce3bc21400a7862c6d6
santinic <hireclaudio@gmail.com>
2022-08-20 12:04:15 +0100

    fixed tests for superuser.com

--- test/google-search.js
+++ test/google-search.js
@@ -4,6 +4,7 @@ const { googleSearch } = require('../lib/how2')
 describe('search', () => {
   it('should find tail -f', (done) => {
     googleSearch('read file while is changing', undefined).then((results) => {
+      assert.isAbove(results.length, 5)
       assert.include((results.map(r => r.snippet).join(' | ')), 'tail -f')
       done()
     }).catch((err) => done(err))
--- test/parse-question-link.js
+++ test/parse-question-link.js
@@ -16,15 +16,16 @@ describe('parseStackoverflowQuestionId', () => {
       questionId: '39934'
     })
   })
-  it('should parse unix.stackoverflow links', () => {
-    const url = 'http://unix.stackexchange.com/questions/39934/is-it-possible-to'
+  it('should parse superuser.com links', () => {
+    const url = 'https://superuser.com/questions/480950/how-to-decompress-a-bz2-file'
+    console.log(fun(url))
     assert.deepEqual(fun(url), {
-      site: 'unix',
-      questionId: '39934'
+      site: 'superuser',
+      questionId: '480950'
     })
   })
   it('should return nul on links without id', () => {
-    const url = 'http://unix.stackexchange.com/questions/tagged/read?sort=votes&pageSize=50'
+    const url = 'https://superuser.com/questions/questions/tagged/read?sort=votes&pageSize=50'
     assert.deepEqual(fun(url), null)
   })
 })
