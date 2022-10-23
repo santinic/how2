f9b32547b5790d976178aed0b5bee9cac2dfb47d
santinic <hireclaudio@gmail.com>
2022-10-23 19:02:35 +0100

    sign apple silicon

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ webpack --config webpack.config.js  # Create build.js
 
 # Build binaries
 rm dist/binaries/*
-pkg build.js -o dist/binaries/how2 --targets=node18-linux,node18-macos,node18-macos-arm64 #,node16-win-x64
-pkg build.js -o dist/binaries/how2 --targets=node18-win-x64
+pkg build.js -o dist/binaries/how2 --targets=node16-linux,node16-macos,node16-macos-arm64 #,node16-win-x64
+pkg build.js -o dist/binaries/how2 --targets=node16-win-x64
 cd dist/binaries
 tar cvzf how2-linux-x64.tar.gz    how2-linux-x64
 tar cvzf how2-macos-x64.tar.gz    how2-macos-x64
@@ -13,6 +13,10 @@ tar cvzf how2-macos-arm64.tar.gz  how2-macos-arm64
 zip -r how2-win.zip               how2.exe
 mv *.tar.gz ../release
 mv *.zip ../release
+
+# Sign Apple Silicon binary
+chmod +x how2-macos-arm64
+codesign -s - -f -vvvvvvv --arch arm64 how2-macos-arm64
 
 # Build .deb
 cd ../deb
