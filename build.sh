8e57eefca312d6d4c00ae374e5c02fe80ee2227d
santinic <hireclaudio@gmail.com>
2022-10-27 14:47:44 +0100

    codesign

--- build.sh
+++ build.sh
@@ -6,17 +6,20 @@ webpack --config webpack.config.js  # Create build.js
 rm dist/binaries/*
 pkg build.js -o dist/binaries/how2 --targets=node16-linux,node16-macos,node16-macos-arm64 #,node16-win-x64
 pkg build.js -o dist/binaries/how2 --targets=node16-win-x64
+
 cd dist/binaries
+
+# Sign Apple Silicon binary
+codesign -vvvvv -s MMJ44P3H29 how2-macos-arm64
+chmod +x how2-macos-arm64
+
+# Compress
 tar cvzf how2-linux-x64.tar.gz    how2-linux-x64
 tar cvzf how2-macos-x64.tar.gz    how2-macos-x64
 tar cvzf how2-macos-arm64.tar.gz  how2-macos-arm64
 zip -r how2-win.zip               how2.exe
 mv *.tar.gz ../release
 mv *.zip ../release
-
-# Sign Apple Silicon binary
-codesign -vvvvv -s MMJ44P3H29 how2-macos-arm64
-chmod +x how2-macos-arm64
 
 # Build .deb
 cd ../deb
@@ -32,5 +35,3 @@ ls -lah
 open ../release
 
 # brew audit --strict --new-formula --online <formula>
-
-
